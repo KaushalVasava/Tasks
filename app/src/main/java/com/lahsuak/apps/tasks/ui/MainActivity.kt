8cdc9079a586ef530bed2ff83f248137ae67b712
KaushalVasava <vasavakaushal333@gmail.com>
2025-08-17 15:10:00 +0530

    temp change

--- app/src/main/java/com/lahsuak/apps/tasks/ui/MainActivity.kt
+++ app/src/main/java/com/lahsuak/apps/tasks/ui/MainActivity.kt
@@ -133,6 +133,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Tasks)
+
         enableEdgeToEdge() // Add this line.
 
         activityContext = this
